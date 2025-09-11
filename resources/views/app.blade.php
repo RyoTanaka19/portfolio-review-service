dcd753a3b12b5d11e4835a8e6f3aa1a31f85d3ec
ryo88 <ryo98tana@gmail.com>
2025-09-11 20:29:22 +0900

    add:ファビコンの追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- ファビコン -->
+        <link rel="icon" type="image/png" href="{{ asset('image/favicon.png') }}">
 
         <!-- Scripts -->
         @routes
