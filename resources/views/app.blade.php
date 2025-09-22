91bdf06db44252d51d9ece4e188f66d532c8a573
ryo88 <ryo98tana@gmail.com>
2025-09-23 08:00:49 +0900

    fix:OGP画像

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,13 +12,14 @@
         <meta property="og:title" content="{{ $page['title'] ?? 'ポートフォリオ' }}">
         <meta property="og:description" content="{{ $page['description'] ?? 'こちらのポートフォリオをご覧ください。' }}">
         <meta property="og:url" content="{{ url()->current() }}">
-        <meta property="og:image" content="{{ asset('images/ogp.png') }}"> <!-- デフォルトOGP画像 -->
+        <meta property="og:image" content="{{ asset('image/ogp.png') }}"> <!-- デフォルトOGP画像 -->
         
         <!-- Twitter Card Tags -->
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:title" content="{{ $page['title'] ?? 'ポートフォリオ' }}">
         <meta name="twitter:description" content="{{ $page['description'] ?? 'こちらのポートフォリオをご覧ください。' }}">
-        <meta name="twitter:image" content="{{ asset('images/ogp.png') }}">
+       <meta name="twitter:image" content="{{ asset('image/ogp.png') }}">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
