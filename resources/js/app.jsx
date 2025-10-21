53026eaaec1bed4faa69ab4fdd9ddc74baf0ab89
ryo88 <ryo98tana@gmail.com>
2025-10-21 19:29:55 +0900

    fix:戻す

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,25 +9,17 @@ import { createRoot } from "react-dom/client";
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
-    // ページタイトルの設定
     title: (title) => `${title} - ${appName}`,
-
-    // ページコンポーネントの解決
     resolve: (name) =>
         resolvePageComponent(
-            // Laravel から渡される名前に対応
             `./Pages/${name}.jsx`,
-            // すべての Pages 以下の jsx/ts/tsx ファイルを事前に登録
-            import.meta.glob("./Pages/**/*.{js,jsx,ts,tsx}", { eager: true })
+            import.meta.glob("./Pages/**/*.jsx")
         ),
-
-    // React 18 の createRoot によるレンダリング
     setup({ el, App, props }) {
         const root = createRoot(el);
+
         root.render(<App {...props} />);
     },
-
-    // プログレスバー設定
     progress: {
         color: "#4B5563",
     },
